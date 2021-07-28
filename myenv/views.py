1473dc4c4e71729d2ae5a43e0c695b0f259c80d5
Akhil Soni <akhil0435@gmail.com>
2021-07-28 12:31:13 +0530

    Adding views

--- myenv/views.py
+++ myenv/views.py
@@ -1,3 +1,17 @@
+from django.http.request import HttpRequest
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+    return HttpResponse("home")
+
+def about(request):
+    return HttpResponse("about")
+
+def blog(request):
+    return HttpResponse("blog")
+
+def contact(request):
+    return HttpResponse("contact")
